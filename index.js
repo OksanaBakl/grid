5e97792c374301f4924d0329b5f931195992f2dc
OksanaBakl <oksana.molesha@gmail.com>
2025-02-09 16:42:46 +0100

    add fn for Array Reduce Transformation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log('Hello, world!');
 /* Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
 
 The fn function takes one or two arguments:
@@ -30,3 +29,38 @@ var filter = function(arr, fn) {
 const arr1 = [0, 10, 20, 30];
 const greaterThan10 = function(n) { return n > 10; };
 console.log(filter(arr1, greaterThan10)); // Output: [20, 30]
+
+
+// 2626. Array Reduce Transformation
+// Easy
+// Companies
+// Hint
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+
+// If the length of the array is 0, the function should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum = init;
+ 
+    for (let i=0; i<nums.length; i++) {
+     accum = fn(accum, nums[i]);
+    }
+
+    return accum;
+ };
+
+ // Example usage:
+const nums = [1, 2, 3, 4];
+const sum = (accum, curr) => accum + curr;
+const init = 0;
+
+console.log(reduce(nums, sum, init)); 
